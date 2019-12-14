d108abad70fc61731fba3d78441909e85c0cb5fc
Lars Behrenberg <32539217+LarsBehrenberg@users.noreply.github.com>
2019-12-14 16:50:30 +0900

    Displaying posts with graphql

--- gatsby-graphql/gatsby-config.js
+++ gatsby-graphql/gatsby-config.js
@@ -6,9 +6,17 @@
 
 module.exports = {
   siteMetadata: {
-    title: `Title from siteMetadata`,
+    title: `Pandas Eating Lots`,
   },
   plugins: [
+    {
+    resolve: `gatsby-source-filesystem`,
+    options: {
+      name: `src`,
+      path: `${__dirname}/src/`,
+    },
+  },
+  `gatsby-transformer-remark`,
     `gatsby-plugin-emotion`,
     {
       resolve: `gatsby-plugin-typography`,
