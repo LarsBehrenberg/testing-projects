0096b4cfd58d2021f3875c67e113bb3ecf3d2318
Lars Behrenberg <32539217+LarsBehrenberg@users.noreply.github.com>
2019-12-16 13:18:33 +0900

    adding seo

--- gatsby-graphql/src/templates/blog-post.js
+++ gatsby-graphql/src/templates/blog-post.js
@@ -1,24 +1,29 @@
-import React from 'react'
-import Layout from '../components/layout'
-import { graphql } from 'gatsby'
+import React from "react"
+import { graphql } from "gatsby"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
 
 export default ({ data }) => {
-    const post = data.markdownRemark
-    return (
-        <Layout>
-            <h1>{post.frontmatter.title}</h1>
-            <div dangerouslySetInnerHTML={{__html: post.html}}></div>
-        </Layout>
-    )
+  const post = data.markdownRemark
+  return (
+    <Layout>
+      <SEO title={post.frontmatter.title} description={post.excerpt} />
+      <div>
+        <h1>{post.frontmatter.title}</h1>
+        <div dangerouslySetInnerHTML={{ __html: post.html }} />
+      </div>
+    </Layout>
+  )
 }
 
 export const query = graphql`
-    query($slug: String!) {
-        markdownRemark(fields: { slug: { eq: $slug } }) {
-            html
-            frontmatter {
-                title
-            }
-        }
+  query($slug: String!) {
+    markdownRemark(fields: { slug: { eq: $slug } }) {
+      html
+      frontmatter {
+        title
+      }
+      excerpt
+    }
   }
 `
